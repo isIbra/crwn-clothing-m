eaa95b165d26b92887d0f46b2af3efb277edda56
isIbra <105174960+isIbra@users.noreply.github.com>
2023-07-24 21:30:34 +0300

    final nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./Components/Routes/home/home";
 import Navigation from "./Components/Routes/Navigation/navigation"
-import { Routes, Route } from "react-router-dom";
+import SignIn from "./Components/Routes/sign-in/sign-in";
+
 
 
 
@@ -14,6 +17,7 @@ const App = () => {
     <Route path='/' element={<Navigation />} > 
       <Route index element={<Home />} />
       <Route path='shop' element={<Shop />} />
+      <Route path='sign-In' element={<SignIn />} />
     </Route>
     </Routes>
   )
